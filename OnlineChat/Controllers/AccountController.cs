4b6d806ed1b0cc45fd46ac14c52b0adbb7f52706
Konstantin Didenko <greybeard.hottabych@gmail.com>
2022-04-20 23:03:32 +0300

    added account controller

---
+++ OnlineChat/Controllers/AccountController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineChat.Mock;
+using OnlineChat.Models;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace OnlineChat.Controllers
+{
+    public class AccountController : Controller
+    {
+        private IRepository _context;
+
+        public AccountController(IRepository context)
+        {
+            _context = context;
+        }
+
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await _context.Users
+                    .Include(u => u.Messages)
+                    .FirstOrDefaultAsync(u => u.NickName == model.NickName);
+                if (user != null)
+                {
+                    await Authenticate(user); // аутентификация
+                    return RedirectToAction("Index", "Account");// переадресация на метод Index
+                }
+                ModelState.AddModelError("", "Bad nickname");
+            }
+            return View(model);
+        }
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login", "Account");
+        }
+        private async Task Authenticate(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, user.NickName),           
+            };
+            ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType,
+                ClaimsIdentity.DefaultRoleClaimType);
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
+        }
+
+    }
+}
